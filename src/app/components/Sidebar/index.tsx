90c341e83370c3193b12694ea7a9f2097c5033c5
matine <mghazanfari@tarasystem.ch>
2024-06-23 15:51:36 +0330

    sidebar

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -30,40 +30,36 @@ export default function Sidebar(props: SidebarInterface) {
     };
 
     return (
-        <div className={'bg'}>
-            <div>
-                {
-                    props.isSidebarOpen &&
-                    <div className={'h-full absolute z-40 top-0 right-0 min-h-200 w-screen bg-secondary-17 bg-opacity-40'}
-                         onClick={handleModalOverlayClick}>
-                        <aside id="separator-sidebar"
-                               className={'h-full z-40 transition-transform sm:translate-x-0 bg-secondary-01 overflow-scroll w-7/12 min-w-60 max-w-80'}
-                               aria-label="Sidebar">
-                            <div className="py-4 overflow-y-auto text-secondary-17 divide-y-2">
-                                <ul className={"h-screen-120 space-y-2 font-medium snap-start snap-y touch-pan-y overflow-y-scroll"}>
-                                    {
-                                        items.map(item => (
-                                            <li key={item.label}>
-                                                <a href={`${item.path}`}
-                                                   className={'flex items-center px-6 py-4 hover:bg-primary-01 ' +
-                                                       'hover:text-primary hover:border-r hover:border-r-2 hover:border-primary ' +
-                                                       'group active:text-primary active:border-r active:border-r-2 active:border-primary'}>
-                                                    <span className="ms-3 group-hover:font-semibold">{item.label}</span>
-                                                </a>
-                                            </li>
-                                        ))
-                                    }
-                                </ul>
-                            </div>
-                        </aside>
-                    </div>
-                }
-            </div>
-            <div>
+        <>
+            {
+                props.isSidebarOpen &&
+                <div className={'bg-gray-500 h-full absolute z-40 top-0 right-0 min-h-200 w-screen bg-secondary-17 bg-opacity-40'}
+                     onClick={handleModalOverlayClick}>
+                    <aside id="separator-sidebar"
+                           className={'h-full z-40 bg-white transition-transform sm:translate-x-0 bg-secondary-01 overflow-scroll w-7/12 min-w-60 max-w-80'}
+                           aria-label="Sidebar">
+                        <div className="py-4 overflow-y-auto text-secondary-17 divide-y-2">
+                            <ul className={"h-screen-120 space-y-2 font-medium snap-start snap-y touch-pan-y overflow-y-scroll"}>
+                                {
+                                    items.map(item => (
+                                        <li key={item.label}>
+                                            <a href={`${item.path}`}
+                                               className={'flex items-center px-6 py-4 hover:bg-primary-01 ' +
+                                                   'hover:text-primary hover:border-r hover:border-r-2 hover:border-primary ' +
+                                                   'group active:text-primary active:border-r active:border-r-2 active:border-primary'}>
+                                                <span className="ms-3 group-hover:font-semibold">{item.label}</span>
+                                            </a>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </aside>
+                </div>
+            }
+            <div className={'h-screen flex flex-col'}>
                 {props.children}
             </div>
-        </div>
-
-
+        </>
     )
 }
