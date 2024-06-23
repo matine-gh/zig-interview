20fa9960303d02c9bb42311d284a66ba882486cc
matine <mghazanfari@tarasystem.ch>
2024-06-23 18:15:05 +0330

    [feat]-general view

--- src/app/components/common/GeneralView/index.tsx
+++ src/app/components/common/GeneralView/index.tsx
@@ -1,10 +1,13 @@
 import KeyboardBackspaceOutlinedIcon from "@mui/icons-material/KeyboardBackspaceOutlined";
 import {GeneralViewInterface} from "@/app/components/common/GeneralView/generalView.interface";
+import {IconButton} from "@mui/material";
 
 export default function GeneralView({text}: GeneralViewInterface) {
     return (
         <div className={'text-2xl space-x-3 p-4'}>
-            <a href={'/'}><KeyboardBackspaceOutlinedIcon /></a>
+            <IconButton href={'/'}>
+                <KeyboardBackspaceOutlinedIcon />
+            </IconButton>
             <b>{text}</b>
         </div>
     )
