319f5831d04faab362f6b29137721b98ea8226e8
matine <mghazanfari@tarasystem.ch>
2024-06-25 02:02:12 +0330

    fix

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,6 +1,5 @@
 'use client'
 import {SidebarInterface} from "./sidebar.interface";
-import Header from "@/app/components/Header";
 
 export default function Sidebar(props: SidebarInterface) {
 
@@ -38,15 +37,15 @@ export default function Sidebar(props: SidebarInterface) {
                     <aside id="separator-sidebar"
                            className={'h-full z-40 bg-white transition-transform sm:translate-x-0 bg-secondary-01 overflow-scroll w-7/12 min-w-60 max-w-80'}
                            aria-label="Sidebar">
-                        <div className="py-4 overflow-y-auto text-secondary-17 divide-y-2">
+                        <div className="py-4 overflow-y-auto text-secondary-light divide-y-2">
                             <ul className={"h-screen-120 space-y-2 font-medium snap-start snap-y touch-pan-y overflow-y-scroll"}>
                                 {
                                     items.map(item => (
                                         <li key={item.label}>
                                             <a href={`${item.path}`}
                                                className={'flex items-center px-6 py-4 hover:bg-primary-01 ' +
-                                                   'hover:text-primary hover:border-r hover:border-r-2 hover:border-primary ' +
-                                                   'group active:text-primary active:border-r active:border-r-2 active:border-primary'}>
+                                                   'hover:text-primary hover:border-r-2 hover:border-primary ' +
+                                                   'group active:text-primary active:border-r-2 active:border-primary'}>
                                                 <span className="ms-3 group-hover:font-semibold">{item.label}</span>
                                             </a>
                                         </li>
